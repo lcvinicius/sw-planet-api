5094086f8bf9b5f0729eedd8dbf404e327598e29
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-14 10:19:38 -0300

    Project created

---
+++ src/main/java/com/example/swplanet/SwPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.example.swplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwPlanetApplication.class, args);
+	}
+
+}
