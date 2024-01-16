54c9c3dc5c3144be4983bbb38c487e366a6b995f
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-16 11:08:14 -0300

    Using Mockito for unit testing

--- src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
@@ -2,19 +2,29 @@ package com.example.swplanet.domain;
 
 import static com.example.swplanet.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = PlanetService.class)
+
+
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
-    @Autowired
+    @InjectMocks
     private PlanetService planetService;
+
+    @Mock
+    private PlanetRepository planetRepository;
 
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet(){
-       Planet sut = planetService.create(PLANET);
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+     
+        Planet sut = planetService.create(PLANET);
        
     assertThat(sut).isEqualTo(PLANET);
     }
