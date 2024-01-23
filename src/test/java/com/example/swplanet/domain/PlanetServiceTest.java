10ea81cc8c29e49da83e1391b532743739284264
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-23 10:36:38 -0300

    Testing the planet query by id

--- src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -40,4 +42,23 @@ public class PlanetServiceTest {
         
     }
     
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);  
+
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
+    }
 }
