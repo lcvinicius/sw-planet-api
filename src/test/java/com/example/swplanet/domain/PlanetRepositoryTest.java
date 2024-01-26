c53c6aebf3753ebfc28d201266374d5c762ea1ab
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-26 08:19:05 -0300

    Testing the success scenario in the repository

---
+++ src/test/java/com/example/swplanet/domain/PlanetRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.swplanet.domain;
+
+import static com.example.swplanet.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithInvalidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+}
