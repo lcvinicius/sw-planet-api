7eaa2f520df8320f6e12024cf27a848962cbf858
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-26 03:49:34 -0300

    Testing planet removal

--- src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanet/domain/PlanetServiceTest.java
@@ -3,8 +3,10 @@ package com.example.swplanet.domain;
 import static com.example.swplanet.common.PlanetConstants.PLANET;
 import static com.example.swplanet.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -65,6 +67,7 @@ public class PlanetServiceTest {
 
         assertThat(sut).isEmpty();
     }
+
     @Test
     public void getPlanet_ByExistingName_ReturnsPlanet(){
         when(planetRepository.findByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
@@ -76,6 +79,7 @@ public class PlanetServiceTest {
 
 
     }
+
     @Test
     public void getPlanet_ByUnexistingName_ReturnsEmpty(){
         final String name = "Unexisting name";
@@ -86,6 +90,7 @@ public class PlanetServiceTest {
         assertThat(sut).isEmpty();
 
     }
+
     @Test
      public void listPlanets_ReturnsAllPlanets(){
         List<Planet> planets = new ArrayList<>(){
@@ -93,6 +98,7 @@ public class PlanetServiceTest {
                 add(PLANET);
         }
     };
+
     Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
     when(planetRepository.findAll(query)).thenReturn(planets);
 
@@ -102,6 +108,7 @@ public class PlanetServiceTest {
     assertThat(sut).hasSize(1);
     assertThat(sut.get(0)).isEqualTo(PLANET);
     }
+
     @Test
     public void listPlanets_ReturnsNoPlanets(){
         when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
@@ -109,6 +116,19 @@ public class PlanetServiceTest {
         List<Planet> sut = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
 
         assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void removePlanet_WithExistingId_doesNotThrowAnyException(){
+    assertThatCode(() -> planetService.remove(1L)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void removePlanet_WithUnexistingId_throwsException(){
+        doThrow(new RuntimeException()).when(planetRepository).deleteById(99L);
+
+        assertThatThrownBy(() -> planetService.remove(99L)).isInstanceOf(RuntimeException.class);
+
     }
 
 }
