f53348e59e877d3ef875968ac932d1caafbbbe8e
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-29 18:36:50 -0300

    Application server configuration

---
+++ src/test/java/com/example/swplanet/PlanetIT.java
@@ -0,0 +1,14 @@
+package com.example.swplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PlanetIT {
+
+    @Test
+    public void contextLoads() {     
+    }
+  
+}
