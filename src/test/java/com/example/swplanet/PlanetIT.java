4f0c6d5b1b5528a65f219bea7721fcfc3b0d26b3
Lucas Vinícius <lucasvsn19@gmail.com>
2024-01-29 18:47:35 -0300

    MySQL database configuration: test profiles

--- src/test/java/com/example/swplanet/PlanetIT.java
+++ src/test/java/com/example/swplanet/PlanetIT.java
@@ -3,7 +3,9 @@ package com.example.swplanet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
 
